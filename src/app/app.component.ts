0cdc345f000682a707124b87f0af6705d37c0ab4
Pandukumar9 <pandukumar.gujja@gmail.com>
2024-12-17 08:55:07 +0530

    imag optimize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactFormComponent } from './forms/react-form/react-form.component';
+import { TemplateFormComponent } from './forms/template-form/template-form.component';
+import { IMAGE_CONFIG, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactFormComponent],
+  imports: [ReactFormComponent,TemplateFormComponent,NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        placeholderResolution: 40
+      }
+    },
+  ],
 })
 export class AppComponent {
   title = 'angular_19_concepts';
--- src/app/forms/template-form/template-form.component.ts
+++ src/app/forms/template-form/template-form.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Actor } from '../actor.model';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'app-template-form',
-  imports: [],
+  selector: 'template-form',
+  imports: [FormsModule,NgFor],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.scss'
 })
 export class TemplateFormComponent {
+
+  skills = ['Method Acting', 'Singing', 'Dancing', 'Swordfighting'];
+  model = new Actor(18, 'Tom Cruise', this.skills[3], 'CW Productions');
+  submitted = false;
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newActor() {
+    this.model = new Actor(42, '', '');
+  }
 
 }
